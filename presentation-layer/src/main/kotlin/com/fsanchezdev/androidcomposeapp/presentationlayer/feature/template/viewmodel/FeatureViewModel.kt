99cc6dc6d1e3129e626e9df1d878887fe4b64aab
Francisco Sanchez Mayorga <franciscosanchezmayorga@gmail.com>
2024-02-24 18:15:52 +0000

    feat: compose base state

--- presentation-layer/src/main/kotlin/com/fsanchezdev/androidcomposeapp/presentationlayer/feature/template/viewmodel/FeatureViewModel.kt
+++ presentation-layer/src/main/kotlin/com/fsanchezdev/androidcomposeapp/presentationlayer/feature/template/viewmodel/FeatureViewModel.kt
@@ -1,8 +1,13 @@
 package com.fsanchezdev.androidcomposeapp.presentationlayer.feature.template.viewmodel
 
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fsanchezdev.androidcomposeapp.domainlayer.FailureBo
+import com.fsanchezdev.androidcomposeapp.presentationlayer.base.compose.BaseMviViewModel
+import com.fsanchezdev.androidcomposeapp.presentationlayer.base.compose.ComposeStateType
+import com.fsanchezdev.androidcomposeapp.presentationlayer.feature.template.state.FeatureEffectEvents
 import com.fsanchezdev.androidcomposeapp.presentationlayer.feature.template.state.FeatureState
+import com.fsanchezdev.androidcomposeapp.presentationlayer.feature.template.state.FeatureStateOld
+import com.fsanchezdev.androidcomposeapp.presentationlayer.feature.template.state.FeatureUserEvents
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,12 +15,16 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 @HiltViewModel
-internal class FeatureViewModel @Inject constructor(
-    private val savedStateHandle: SavedStateHandle
-) : ViewModel() {
+internal class FeatureViewModel @Inject constructor() : BaseMviViewModel<
+    FeatureUserEvents,
+    FeatureEffectEvents,
+    FeatureState
+    >(
+    initialState = FeatureState()
+) {
 
     private val _state =
-        MutableStateFlow(FeatureState())
+        MutableStateFlow(FeatureStateOld())
     val state = _state.asStateFlow()
 
     fun greet(name: String) {
@@ -24,5 +33,18 @@ internal class FeatureViewModel @Inject constructor(
                 greeting = "Hello, $name"
             )
         }
+    }
+
+    override fun onEvent(event: FeatureUserEvents) {
+        when (event) {
+            is FeatureUserEvents.OnButtonClicked -> sendEffect {
+                FeatureEffectEvents.ShowGreetings(event.buttonTextGreeting)
+            }
+        }
+    }
+
+    private fun handleError(error: FailureBo) {
+        emitState { copy(type = ComposeStateType.Render) }
+        viewModelScope.launchFailure(error)
     }
 }
