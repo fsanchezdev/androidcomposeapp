71d2238bc28c2519b6e7eae9225968daf52beb28
Francisco Sanchez Mayorga <franciscosanchezmayorga@gmail.com>
2024-03-13 18:18:00 +0000

    feat: ksp instead kapt

--- data-layer/build.gradle.kts
+++ data-layer/build.gradle.kts
@@ -1,4 +1,3 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
@@ -10,7 +9,6 @@ android {
 
     defaultConfig {
         minSdk = 29
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
